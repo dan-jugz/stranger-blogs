503eed1aac5edec345c9334fd7076c17a8e0808b
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-09 12:14:07 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://daniel:watchlist@localhost/pitch'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")    
+
+class DevConfig(Config):
+        DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+}
