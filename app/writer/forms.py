3b00463b87a51bef87fd03c39649846721ee59d7
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-10 16:26:19 +0300

    add a classes on the form script for a new blog and editing your blog

--- app/writer/forms.py
+++ app/writer/forms.py
@@ -1,3 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import Required
+
+class NewBlogForm(FlaskForm):
+    title = StringField('Title', validators=[Required()], render_kw={"placeholder": "Short but precise"})
+    writer_url = StringField('Link to your website(optional)', render_kw={"placeholder": "To easily navigate readers to your website"})
+    body = TextAreaField('Blog Body', render_kw={"placeholder": "Idealy should have atleast 300 characters"})
+    submit = SubmitField('Create blog')
+
+
+class EditBlogForm(FlaskForm):
+    title = StringField('Change title')
+    writer_url = StringField('Change link to your website')
+    body = TextAreaField('New Body?')
+    submit = SubmitField('Save changes')
