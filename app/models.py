b2bfe093ff99288bd6147877742f637d458fd7f4
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-10 18:46:03 +0300

    add a new writer class

--- app/models.py
+++ app/models.py
@@ -1,25 +1,39 @@
-from flask import Flask
-from datetime import datetime
 from . import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import login_manager
+from datetime import datetime
 
-#Creating table that holds user information
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return Writer.query.get(int(user_id))
 
-    __tablename__ = 'users'
+class Writer(UserMixin,db.Model):
+    """
+    This class allows us to have a writers table that has the following columns:
+        1. id
+        2. writer_name
+        3. email
+        4. password
+        5. writer_blog
+    """
+    __tablename__ = 'writers'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50))
-    username = db.Column(db.String(20))
-    password = db.Column(db.String(20))
-    password = db.Column(db.String(20))
+    writer_name = db.Column(db.String(50))
+    email = db.Column(db.String)
+    password_hash = db.Column(db.String(255))   
+    writer_blog = db.relationship('Blog', backref="writer", lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('Access denied')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
-#Creating table that holds the blog information
-class Blogpost(db.Model): 
-
-    __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    subtitle = db.Column(db.String(50))
-    author = db.Column(db.String(20))
-    content = db.Column(db.String(500))
-    date_posted = db.Column(db.DateTime)
+    def __repr__(self):
+        return "Writer {}".format(self.writer_name)
