3195d026e3201d2760417cd43f0301acd76294ba
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-10 12:43:01 +0300

    use the api given to generate a random quote and destructure it from json format. add a page to subscribe for new users

--- app/request.py
+++ app/request.py
@@ -5,14 +5,22 @@ from .models import User
 User=user.User
 
 #random quote generator
-base_url=app.config['QUOTES_URL']
+api_key=app.config['QUOTE_URL']
 
-def get_quotes():
+def get_random_quote():
     '''
-    Function that gets the json response to our url request
+    Function that gets the random quote
     '''
-    get_quotes_url = base_url
+    get_quote_url = base_url
 
-    with urllib.request.urlopen(get_quotes_url) as url:
-        get_quotes_data = url.read()
-        get_quotes_response = json.loads(get_quotes_data)
+    with urllib.request.urlopen(api_key) as url:
+        get_quote_data = url.read()
+        quote_json = json.loads(get_quote_data)
+
+        quote_result = None
+
+        if quote_json:
+            qoute_item = quote_json
+            quote_result = process_quote_data(qoute_item)
+
+    return quote_result
--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,29 @@
 from flask import render_template, flash
 from . import user
-
+from request import get_random_quote
 
 @user.route('/')
 def home():
     title = 'Stranger Blogs'
+    random_quote = get_random_quote()
 
-    return render_template('index.html',title=title)
+    return render_template('user/home/home.html', title=title, random_quote=random_quote)
+
+@user.route('/subscribe', methods=['GET', 'POST'])
+def subscribe():
+    title = "Stranger | Subscribe"
+    form = forms.SubscribeForm()
+    if form.validate_on_submit():
+        if Subscriber.query.filter_by(email=form.email.data).first():
+            flash('You are already subscribed')
+        else:
+            new_subscriber = Subscriber(email=form.email.data)
+            db.session.add(new_subscriber)
+            db.session.commit()
+            subscribers = Subscriber.query.all()
+            print("Subscribers are:", subscribers)
+            for i in subscribers:
+                mail_message("Thank you for subscribing", "email/thank_you", i.email)
+            flash('Successfully subscribed!')
+    
+    return render_template('subscribe.html', form=form)
