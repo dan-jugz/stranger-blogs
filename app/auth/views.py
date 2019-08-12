cad77ba449dfbc7d1af70aea61a3ff40fd40a6a4
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-12 09:46:05 +0300

    add the route to direct user to the login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,15 @@ from ..models import Writer
 from .forms import RegistrationForm, LoginForm
 from .. import db
 from ..writer import writer_user
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    title = 'Stranger | Writer Login'
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        writer = Writer.query.filter_by(email=login_form.email.data).first()
+        if writer is not None and writer.verify_password(login_form.password.data):
+            login_user(writer, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('writer_user.dashboard'))
+        flash('Invalid Writer_name or Password')
+
