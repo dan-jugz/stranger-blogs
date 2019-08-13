883ec781330e1edd86441be78e54ded4adfbf718
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-13 03:18:06 +0300

    import the classes from model to inject them into the manager shell

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
+from app.models import Writer,Blog,Comment,Subscriber
 
 #create app instance
 app = create_app('development')
@@ -14,7 +15,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict (app=app,db=db)
+    return dict (app=app,db=db,Writer=Writer,Blog=Blog,Comment=Comment,Subscriber=Subscriber)
 
 if __name__=='__main__':
     manager.run()
